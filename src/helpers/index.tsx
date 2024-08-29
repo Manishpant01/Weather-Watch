a688cf6ed334673218943e2a12d2166539333f8a
Manish Pant <manishpant203@gmail.com>
2024-08-29 16:03:02 +0530

    feat: export helper function

---
+++ src/helpers/index.tsx
@@ -0,0 +1,12 @@
+import getWeatherImage from "./getWeatherImage";
+import { getWeatherDescription } from "./getWeatherImage";
+import { DEFAULT_LOCATION } from "./constants";
+import { formatDate, formateDate } from "./getFormatDate";
+
+export {
+  getWeatherDescription,
+  getWeatherImage,
+  DEFAULT_LOCATION,
+  formatDate,
+  formateDate,
+};
