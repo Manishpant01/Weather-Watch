f8948c5c55b4e40d2fc8621146590689d3990f6d
Manish Pant <manishpant203@gmail.com>
2024-08-29 13:52:18 +0530

    feat: update implementation

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,11 @@
-import { FC, useEffect, useState } from "react";
-import { ImageBackground, SafeAreaView, StyleSheet, View } from "react-native";
+import React, { FC, useCallback, useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  View,
+} from "react-native";
 import { LocationList, SearchInput } from "../components";
 import { DEFAULT_LOCATION } from "../helpers/constants";
 import { geocoordinateResult, getGeocoordinate } from "../services";
@@ -7,22 +13,57 @@ import Weather from "./weather";
 
 const Main: FC = () => {
   const [locations, setLocations] = useState<geocoordinateResult[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(true);
+
+  const fetchCoordinates = async (location: string) => {
+    try {
+      const currentLocationData = await getGeocoordinate(location);
+      setLocations(currentLocationData.results || []);
+      console.log(currentLocationData.results);
+    } catch (error) {
+      console.error("Error fetching coordinates:", error);
+      setLocations([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getCoordinate = useCallback((location: string) => {
+    fetchCoordinates(location);
+  }, []);
 
   useEffect(() => {
     getCoordinate(DEFAULT_LOCATION);
-  }, []);
+  }, [getCoordinate]);
 
-  const getCoordinate = async (location: string) => {
-    const currentLocationData = await getGeocoordinate(location);
-    setLocations(currentLocationData.results);
-  };
+  const setSelectedLocation = useCallback(
+    (selectedLocation: geocoordinateResult[]) => {
+      setLocations(selectedLocation);
+    },
+    []
+  );
 
-  const setSelectedLocation = (selectedLocation: geocoordinateResult[]) => {
-    setLocations(selectedLocation);
-  };
+  const LoadingView = () => (
+    <View style={[styles.containerLoading, styles.horizontal]}>
+      <ActivityIndicator size="large" color="#87CEEB" />
+    </View>
+  );
+
+  const showWeatherView = () => (
+    <>
+      {locations.length === 0 || locations.length > 1 ? (
+        <LocationList
+          updateLocation={setSelectedLocation}
+          location={locations}
+        ></LocationList>
+      ) : (
+        <Weather locations={locations.length ? locations : null} />
+      )}
+    </>
+  );
 
   return (
-    <>
+    <View testID="main-component" style={styles.container}>
       <ImageBackground
         source={require("../imgs/weather.jpg")}
         resizeMode="cover"
@@ -33,22 +74,18 @@ const Main: FC = () => {
             <View style={styles.mainWeatherContainer}>
               <SearchInput getCoordinate={getCoordinate}></SearchInput>
             </View>
-            {locations?.length > 1 ? (
-              <LocationList
-                updateLocation={setSelectedLocation}
-                location={locations}
-              ></LocationList>
-            ) : (
-              <Weather locations={locations} />
-            )}
+            {isLoading ? <LoadingView /> : showWeatherView()}
           </SafeAreaView>
         </View>
       </ImageBackground>
-    </>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   mainWeatherContainer: {
     marginLeft: 30,
     marginRight: 30,
@@ -66,6 +103,16 @@ const styles = StyleSheet.create({
   overlay: {
     flex: 1,
     backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+
+  containerLoading: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
   },
 });
 
